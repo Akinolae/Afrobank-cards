b7f65dd7dcb31b664ed4054155047fa6f7fcd2c2
Akinolae <makindeakinola22@gmail.com>
2024-07-09 19:10:47 +0100

    Added new changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ import { authImpl } from '../services/auth.js'
 
 const router = express.Router()
 
-router.get('/get-cards', cardsImpl.getCards)
-router.post('/create-card', cardsImpl.createCard)
-router.get('/get-api-key', authImpl.genAPIkey)
+router.get('/get-cards', authImpl.verifyUser, cardsImpl.getCards)
+router.post('/create-card', authImpl.verifyUser, cardsImpl.createCard)
+router.get('/get-api-key', authImpl.verifyUser, authImpl.genAPIkey)
 
 export { router }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,7 @@
 import KeyManager from './apiKeyManager.js'
 import { response } from './responseHandler.js'
+import { StatusCodes } from 'http-status-codes'
+import { getValueFromAttributes, getUserFromCognito } from '../../lib/aws.js'
 
 class AuthImpl extends KeyManager {
   constructor() {
@@ -14,6 +16,54 @@ class AuthImpl extends KeyManager {
       response({ message: api_key, success: true, code: 200, res })
     } catch (error) {
       response({ message: error.message, success: false, code: 500, res })
+    }
+  }
+
+  verifyUserAccessToken = async (token) => {
+    try {
+      const data = await getUserFromCognito(token)
+      return data
+    } catch (error) {
+      throw error
+    }
+  }
+
+  verifyUser = async (req, res, next) => {
+    try {
+      const authToken = req.headers.authorization
+
+      const token = !!authToken ? authToken.split(' ')[1] : ''
+      const verify = await this.verifyUserAccessToken(token)
+      let user = {}
+      user.user_id = getValueFromAttributes(
+        verify.UserAttributes,
+        'custom:user_id'
+      )
+      user.email = getValueFromAttributes(verify.UserAttributes, 'email')
+
+      !verify
+        ? response({
+            message: 'user not found',
+            success: false,
+            code: StatusCodes.UNPROCESSABLE_ENTITY,
+            res,
+          })
+        : (res.user = user)
+      next()
+    } catch (error) {
+      const err = error.toString()
+      console.log({ err })
+      const errMessage = err.includes('invalid')
+        ? error
+        : err.includes('expired')
+        ? 'Token expired'
+        : 'no token provided'
+      response({
+        message: errMessage,
+        success: false,
+        code: StatusCodes.UNAUTHORIZED,
+        res,
+      })
     }
   }
 }
